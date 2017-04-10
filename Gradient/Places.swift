8b59f63303216b22ba640bc9c3d547cfd980439a
Julian Bossiere <julianbossiere@Julians-MacBook-Pro.local>
2017-04-10 00:51:17 -0700

    added example of polylines and polygons using data from plist

---
+++ Gradient/Places.swift
@@ -0,0 +1,13 @@
+//
+//  Places.swift
+//  Gradient
+//
+//  Created by Julian Bossiere on 4/9/17.
+//  Copyright © 2017 Julian Bossiere. All rights reserved.
+//
+
+import UIKit
+
+class Places: NSObject {
+
+}
